ecc57bd1f3f8954073a95a0a7bff94cc44dc8dec
Vincenzo <laveneziana.vincenzo@gmail.com>
2020-04-14 17:54:29 +0200

    Fine esercizio

--- js/main.js
+++ js/main.js
@@ -7,12 +7,14 @@ $(document).ready(function () {
   var dropDown = $(".action_list .actions");
   var dropLinks = dropDown.children("a");
   var dropMenu = dropDown.children(".drop_down");
+  var header = $("header");
 
   //show and hide dropDown menu
 
   dropLinks.mouseenter(function () { 
 
     var actualMenu = $(this).next(".drop_down");
+
 
     dropMenu.hide();
 
@@ -22,10 +24,10 @@ $(document).ready(function () {
   });
 
 
-
-  dropMenu.mouseleave(function () {
+  header.mouseleave(function () {
     dropMenu.hide()
   })
+
 
   
   
