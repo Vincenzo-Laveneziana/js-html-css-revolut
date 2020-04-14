ee960e110afdbf6fe5b6bd67059c3c546180e27c
Vincenzo <laveneziana.vincenzo@gmail.com>
2020-04-14 17:07:00 +0200

    Fine logica js. Sistemazione generale dello style

--- js/main.js
+++ js/main.js
@@ -1,11 +1,34 @@
 $(document).ready(function () {
   
 
-  console.log("Funziona");
+  //Dropdown menu
+  //referenze
+
+  var dropDown = $(".action_list .actions");
+  var dropLinks = dropDown.children("a");
+  var dropMenu = dropDown.children(".drop_down");
+
+  //show and hide dropDown menu
+
+  dropLinks.mouseenter(function () { 
+
+    var actualMenu = $(this).next(".drop_down");
+
+    dropMenu.hide();
+
+    actualMenu.toggle()
+    
+  
+  });
+
+
+
+  dropMenu.mouseleave(function () {
+    dropMenu.hide()
+  })
+
+  
   
 
 
-
-
-
-});
+});//fine ready
